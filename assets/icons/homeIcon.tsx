cf8471bf756d14f26b3b3171b3bf5fadfb91572d
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-07-05 19:40:09 +0300

    fix tab icons

--- assets/icons/homeIcon.tsx
+++ assets/icons/homeIcon.tsx
@@ -12,66 +12,66 @@ export const HomeIcon: FC<IProps> = ({ width, height, color }) => (
         <Path
             d="M8.52505 32.0113L41.1009 11.6302"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M73.9591 32.0113L41.3832 11.6302"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M18.4615 66.5067V26.6478"
             stroke={color || "#FEFBF9"} 
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M63.458 66.5067V25.7445"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M19.252 66.5067H63.458"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M51.5455 8.52505V17.7841"
             stroke={color || "#FEFBF9"} 
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M58.885 8.52505V22.5829"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <Path
             d="M51.5455 8.52505H58.4333"
             stroke={color || "#FEFBF9"}
-            stroke-width="7"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
     </Svg>
 )
