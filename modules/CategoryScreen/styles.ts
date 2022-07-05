db584a173a694e67da7e27e556e726adab60bfda
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-07-05 19:48:43 +0300

    fix main page styles

--- modules/CategoryScreen/styles.ts
+++ modules/CategoryScreen/styles.ts
@@ -7,6 +7,7 @@ export const getStyle = (colors: IColors) => {
         container: {
             flex: 1,
             backgroundColor: colors.background,
+            paddingBottom: 50
         },
     });
     return styles;
